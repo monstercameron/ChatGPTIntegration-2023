410542c70616f4a1760293fe814e3913fa082041
Michael McGarvey Jr <michael.mcgarveyjr@ukg.com>
2023-07-11 19:15:19 -0400

    Added chatbot to employee page

--- Extension/index.js
+++ Extension/index.js
@@ -1,39 +1,22 @@
-window.onload = function () {
-
-  const urlParams = new URLSearchParams(window.location.search);
-  // console.log(window.location.search);
-
-  const eeid = urlParams.get('eeid');
-  // console.log(eeid);
+window.onload = async function () {
 
   // Grabs the right-hand container
   const element = document.getElementById('ContentFrame').contentWindow.document.getElementById('ctl00_thingsICanDoMainDiv')
-  console.log(element);
-  // if (element == null) 
-  // return;
 
-  // Creates a test element
-  const injectElement = document.createElement('div');
-  injectElement.className = 'Test-Div';
-  injectElement.innerHTML = 'EXAMPLE TEST';
-
-  fetch(chrome.runtime.getURL('/index.html'))
+  await fetch(chrome.runtime.getURL('/index.html'))
     .then(response => response.text())
     .then(data => {
+
       const tempVar = document.createElement('div');
       tempVar.className = "Testcase";
       tempVar.innerHTML = data;
-      document.body.appendChild(tempVar);
-      console.log(tempVar);
-      // other code
-      // eg update injected elements,
-      // add event listeners or logic to connect to other parts of the app
-    }).catch(err => {
-      console.error(err);
-      // handle error
-    });
+      element.appendChild(tempVar);
 
-  element.appendChild(injectElement);
+    }).catch(err => {
+
+      console.error(err);
+
+    });
 
   const getTime = () => {
     const now = new Date();
@@ -45,18 +28,18 @@ window.onload = function () {
   }
 
   const addUserChat = () => {
-    const value = document.getElementById('textInput').value;
+    const value = document.getElementById('ContentFrame').contentWindow.document.getElementById('textInput').value;
     addUserChatBubble(value);
   };
 
   const addUserChatBubble = (chatText) => {
-    const chatElement = document.getElementById('chatBox');
-    const newBubble = document.createElement('div');
+    const chatElement = document.getElementById('ContentFrame').contentWindow.document.getElementById('chatBox');
+    const newBubble = document.getElementById('ContentFrame').contentWindow.document.createElement('div');
     newBubble.innerHTML = `<div class="bubbleWrapper">\n      <div class="inlineContainer own">\n        <img class="inlineIcon" src="https://www.pinclipart.com/picdir/middle/205-2059398_blinkk-en-mac-app-store-ninja-icon-transparent.png">\n        <div class="ownBubble own">\n          ${chatText}\n        </div>\n      </div><span class="own">${getTime()}</span>\n    </div>`;
     chatElement.appendChild(newBubble);
   };
 
-  const el = document.getElementById('textInput');
+  const el = document.getElementById('ContentFrame').contentWindow.document.getElementById('textInput');
   console.log(el);
   el.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
