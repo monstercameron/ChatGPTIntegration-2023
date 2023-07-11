549f258764318c8882f63831f545e265c0e97f59
Austin Robertson <austin.robertson@ukg.com>
2023-07-11 18:46:57 -0400

    add res bubble

--- UKG-UP Extension/popup.js
+++ UKG-UP Extension/popup.js
@@ -21,6 +21,14 @@ const addUserChatBubble = (chatText) => {
     newBubble.innerHTML = `<div class="bubbleWrapper">\n      <div class="inlineContainer own">\n        <img class="inlineIcon" src="https://www.pinclipart.com/picdir/middle/205-2059398_blinkk-en-mac-app-store-ninja-icon-transparent.png">\n        <div class="ownBubble own">\n          ${chatText}\n        </div>\n      </div><span class="own">${getTime()}</span>\n    </div>`;
     chatElement.appendChild(newBubble);
 };
+
+const addResponseChatBubble = (chatText) => {
+    const chatElement = document.getElementById('chatBox');
+    const newBubble = document.createElement('div');
+    newBubble.innerHTML = `<div class="bubbleWrapper">\n      <div class="inlineContainer">\n        <img class="inlineIcon" src="https://www.pinclipart.com/picdir/middle/205-2059398_blinkk-en-mac-app-store-ninja-icon-transparent.png">\n        <div class="otherBubble other">\n          ${chatText}\n        </div>\n      </div><span class="other">${getTime()}</span>\n    </div>`;
+    chatElement.appendChild(newBubble);
+};
+
 window.addEventListener("DOMContentLoaded", (event) => {
     const el = document.getElementById('textInput');
       el.addEventListener('keydown', (event) => {
