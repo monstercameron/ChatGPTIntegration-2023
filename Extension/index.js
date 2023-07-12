ca5010499826b1e04945cfd6749d9a3f3c5d693c
Michael McGarvey Jr <michael.mcgarveyjr@ukg.com>
2023-07-12 13:47:30 -0400

    Added additional functionality/styles for chatbox

--- Extension/index.js
+++ Extension/index.js
@@ -56,7 +56,7 @@ window.onload = async function () {
   const addUserChatBubble = (chatText) => {
     const chatElement = document.getElementById('chatBox');
     const newBubble = document.createElement('div');
-    newBubble.innerHTML = `<div class="bubbleWrapper">\n      <div class="inlineContainer own">\n        <img class="inlineIcon" src="https://www.pinclipart.com/picdir/middle/205-2059398_blinkk-en-mac-app-store-ninja-icon-transparent.png">\n        <div class="ownBubble own">\n          ${chatText}\n        </div>\n      </div><span class="own">${getTime()}</span>\n    </div>`;
+    newBubble.innerHTML = `<div class="bubbleWrapper">\n      <div class="inlineContainer own">\n        <img class="inlineIcon" src="https://i.imgur.com/r3TyRAR.png">\n        <div class="ownBubble own">\n          ${chatText}\n        </div>\n      </div><span class="own">${getTime()}</span>\n    </div>`;
     chatElement.appendChild(newBubble);
   };
 
